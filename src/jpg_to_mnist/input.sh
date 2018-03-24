f6da97df498c84d78e99489fb1bc6e85f0af9abe
charleschang <im741314@gmail.com>
2018-03-24 10:11:07 +0800

    script works

--- src/jpg_to_mnist/input.sh
+++ src/jpg_to_mnist/input.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rm -r ./test-images/*
-rm -r ./training-images/*
+rm -r $(dirname $0)/test-images/*
+rm -r $(dirname $0)/training-images/*
 
 
 list=( "glass" "metal" "paper" "plastic")
@@ -9,21 +9,24 @@ count=0
 for name in "${list[@]}"
 do
 
-  
+  echo "copy ${name}" 
   TRAINING_DIR=$(dirname $0)/training-images/${count}
   TEST_DIR=$(dirname $0)/test-images/${count}
+
+
   #populate training images
-  mkdir ./training-images/${count}
-  cp -r ../../data/dataset-resized/${name}/*  ./training-images/${count}/
+  mkdir $TRAINING_DIR
+  cp -r ../../data/dataset-resized/${name}/*  $TRAINING_DIR/
 
   #populate validation images
-  mkdir ./test-images/${count}
+  mkdir $TEST_DIR
 
-
-  ls $TRAINING_DIR |sort -R |tail 50 |while read file; do
+  ls $TRAINING_DIR | sort -R | tail -n 50 | while read file; do
       cp $TRAINING_DIR/$file $TEST_DIR/$file
   done
 
+  #rename the files in test images correctly
+  ls $TEST_DIR | cat -n | while read n f; do mv "$TEST_DIR/$f" "$TEST_DIR/$n.jpg"; done
 
   ((count++))
 done
